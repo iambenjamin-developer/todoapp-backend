2b1217d5457d715e6e192c7b0114dea37bc62e3c
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-04-01 15:38:05 -0300

    Add EF Core

---
+++ WebApi/Infrastructure/Configurations/ToDoItemConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Entities;
+
+namespace WebApi.Infrastructure.Configurations
+{
+     public class ToDoItemConfiguration : IEntityTypeConfiguration<ToDoItem>
+    {
+        public void Configure(EntityTypeBuilder<ToDoItem> builder)
+        {
+            builder.ToTable("ToDoItems");
+
+            builder.HasKey(e => e.Id);
+
+            builder.Property(x => x.Name)
+                  .HasMaxLength(256)
+                  .IsUnicode(false)
+                  .IsRequired();
+
+            builder.Property(x => x.IsCompleted)
+                   .IsRequired();
+        }
+    }
+}
